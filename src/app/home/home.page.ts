cd718169ed2fb933f3a8192617f4c45e3af537ca
syntaxroror <louisvalencia308@yahoo.com>
2025-04-28 03:00:50 +0800

    yes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,7 +7,30 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class HomePage {
+  twentyFive: number = 25*60;
+  isOn: boolean = false;
+  interval: any;
 
   constructor() {}
+  
+  started(){
+    if (this.isOn) return;
+    this.isOn = true;
+    
+    this.interval = setInterval(() => {
+      if (this.twentyFive > 0){
+        this.twentyFive--;
+      } else {
+        clearInterval(this.interval);
+        this.isOn = false;
+        alert('Time is up');
+      }
+    }, 1000);
+  }
 
+  formatTime(): string {
+    const minutes = Math.floor(this.twentyFive / 60);
+    const seconds = this.twentyFive % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+  }
 }
